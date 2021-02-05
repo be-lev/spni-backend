6e909af2ac0d4e335941f6f063f87221da098eec
Lev <levberger19890@gmail.com>
2021-02-05 15:58:07 +0200

    first commit

---
+++ business-logic-layer/spni-logic-layer.js
@@ -0,0 +1,49 @@
+const dal = require("../data-access-layer/dal")
+
+async function getHikingSitesAsync () {
+    const sql = `SELECT siteId, name, description, adultEntryPrice, kidsEntryPrice, h.location 
+    FROM israelhikingsites as I 
+    JOIN hikingsitesareas as H 
+    ON I.locationId= H.locationId`
+
+    const sites = await dal.executeAsync(sql);
+    return sites
+}
+
+async function addSiteAsync ( site) {
+    const sql = `INSERT INTO israelhikingsites VALUES(
+        DEFAULT,
+        '${site.locationId}',
+        '${site.name}',
+        '${site.description}',
+        ${site.adultEntryPrice},
+        ${site.kidsEntryPrice})`; 
+    const info = await dal.executeAsync(sql);
+    site.siteId = info.insertId;
+
+    site.location = await getLocationName(site.locationId)
+} 
+
+async function getLocationName(locationId){
+    const sql = `SELECT location FROM hikingsitesareas WHERE locationId = ${locationId}`;
+    const locations = await dal.executeAsync(sql);
+    return locations[0].location
+}
+
+async function getAllLocationsAsync(){
+    const sql = 'SELECT * FROM hikingsitesareas'
+    const locations = await dal.executeAsync(sql);
+    return locations
+}
+
+async function deleteSiteAsync(siteId){
+    const sql = `DELETE FROM israelhikingsites WHERE siteId = ${siteId}`
+    await dal.executeAsync(sql)
+}
+
+module.exports ={
+    getAllLocationsAsync,
+    addSiteAsync,
+    getHikingSitesAsync,
+    deleteSiteAsync
+}
